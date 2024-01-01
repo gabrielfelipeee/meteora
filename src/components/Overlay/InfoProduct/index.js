586ce3d9291d1072044b71081a024f6219da2978
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-01-01 17:34:29 -0300

    Atualizando as informações dos produtos

--- src/components/Overlay/InfoProduct/index.js
+++ src/components/Overlay/InfoProduct/index.js
@@ -25,7 +25,7 @@ const InfoProduct = ({ image, name, description, price, size, colors, id }) => {
     return (
         <section className="box-info-product">
             <div className="box-head">
-                <img src={circle} alt="imagem de um círculo amarelo"/>
+                <img src={circle} alt="imagem de um círculo amarelo" />
                 <h2>Confira detalhes sobre o produto</h2>
                 <MdClear className="clear" onClick={() => closeDetails()} />
             </div>
@@ -39,37 +39,40 @@ const InfoProduct = ({ image, name, description, price, size, colors, id }) => {
                     <span className="price">R$ {price},00</span>
                     <p className="store">Vendido e entregue por Meteora</p>
 
-                    <fieldset className="radios">
-                        <legend>Cores:</legend>
-                        {colors?.map(color => (
-                            <div key={color}>
-                                <input
-                                    type="radio"
-                                    id={`radio_${color}`}
-                                    name="color"
-                                    value={color}
-                                    onChange={event => changeColor(event)}
-                                />
-                                <label htmlFor={`radio_${color}`}>{color}</label>
-                            </div>
-                        ))}
-                    </fieldset>
-
-                    <fieldset className="radios">
-                        <legend>Tamanhos:</legend>
-                        {size?.map(size => (
-                            <div key={size}>
-                                <input
-                                    type="radio"
-                                    id={`radio_${size}`}
-                                    name="size"
-                                    value={size}
-                                    onChange={event => changeSize(event)}
-                                />
-                                <label htmlFor={`radio_${size}`}>{size}</label>
-                            </div>
-                        ))}
-                    </fieldset>
+                    {
+                        colors && <fieldset className="radios">
+                            <legend>Cores:</legend>
+                            {colors?.map(color => (
+                                <div key={color}>
+                                    <input
+                                        type="radio"
+                                        id={`radio_${color}`}
+                                        name="color"
+                                        value={color}
+                                        onChange={event => changeColor(event)}
+                                    />
+                                    <label htmlFor={`radio_${color}`}>{color}</label>
+                                </div>
+                            ))}
+                        </fieldset>
+                    }
+                    {
+                        size && <fieldset className="radios">
+                            <legend>Tamanhos:</legend>
+                            {size?.map(size => (
+                                <div key={size}>
+                                    <input
+                                        type="radio"
+                                        id={`radio_${size}`}
+                                        name="size"
+                                        value={size}
+                                        onChange={event => changeSize(event)}
+                                    />
+                                    <label htmlFor={`radio_${size}`}>{size}</label>
+                                </div>
+                            ))}
+                        </fieldset>
+                    }
                     <button
                         onClick={() => {
                             addProduct(
